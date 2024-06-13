d2e5a6ad0a1f38accc4c2e37a1b246b380d1de2c
Phillip Weinberg <weinbe58@gmail.com>
2024-06-13 09:32:23 -0400

    stash.

--- src/parallel_sparse_tools/matvec/_oputils.cxx
+++ src/parallel_sparse_tools/matvec/_oputils.cxx
@@ -16,14 +16,9 @@ using array_variants = std::variant<
 
 using index_variants = std::variant<py:array_t<npy_int32>, py::array<npy_int64>>;
 
-using dense_variants = std::variant<py::array_t<npy_bool>,py::array_t<npy_int8>, py::array_t<npy_int16>, py::array_t<npy_int32>, py::array_t<npy_int64>,
-                                     py::array_t<npy_uint8>, py::array_t<npy_uint16>, py::array_t<npy_uint32>, py::array_t<npy_uint64>,
-                                     py::array_t<npy_float32>, py::array_t<npy_float64>, py::array_t<npy_cfloat_wrapper>, py::array_t<npy_cdouble_wrapper>>;
-
-
-array_variants get_array(py::array &arr){
-    if (arr.ndom() != 1){
-        throw std::invalid_argument("Array must be 1D");
+array_variants get_array(py::array &arr, const int ndim = -1, const std::string &name){
+    if (ndim >= 0 && arr.ndim() != ndim){
+        throw std::invalid_argument("Array "+name+" must be "+std::to_string(ndim)+"-dimensional");
     }
     const int type_num = arr.dtype().num();
     switch (expression)
@@ -59,7 +54,10 @@ array_variants get_array(py::array &arr){
     }
 }
 
-index_variants get_index(py::array &arr){
+index_variants get_index(py::array &arr, const std::string &name){
+    if (arr.ndim() != 1){
+        throw std::invalid_argument(name+" array must be 1-dimensional");
+    }
     const int type_num = array.dtype().num();
     switch (expression)
     {
@@ -72,6 +70,44 @@ index_variants get_index(py::array &arr){
     }
 }
 
+
+
+int csr_matvec_runtime(
+    const bool overwrite,
+    const 
+    const npy_intp n_row,
+    py::array py_indptr,
+    py::array py_indices,
+    py::array py_data,
+    py::array py_x,
+    py::array py_y
+) {
+    if (n_row < 0) {
+        throw std::invalid_argument("n_row must be non-negative");
+    }
+    if (n_row == 0) {
+        return;
+    }
+
+    index_variants indptr = get_index(py_indptr, 1, "indptr");
+    index_variants indices = get_index(py_indices, 1, "indices");
+    array_variants data = get_array(py_data, 1, "data");
+    array_variants x = get_array(py_x, -1);
+    array_variants y = get_array(py_y);
+
+    std::visit(
+        [&n_row](const auto& indptr, const auto& indices, const auto& data, const auto &y, auto &x){
+            using indptr_t = typename decltype(indptr)::value_type;
+            using indices_t = typename decltype(indices)::value_type;
+            using data_t = typename decltype(data)::value_type;
+            using y_t = typename decltype(y)::value_type;
+            using x_t = typename decltype(x)::value_type;
+            using result_t = result_type_t<data_t, 
+
+        }
+    )
+
+}
 
 
 
