611cfe44561912153c588cd1d751ba06fbb5822e
Phillip Weinberg <weinbe58@gmail.com>
2024-06-20 07:18:31 -0400

    refactor

--- src/parallel_sparse_tools/matvec/_oputils.cxx
+++ src/parallel_sparse_tools/matvec/_oputils.cxx
@@ -19,12 +19,7 @@ using array_variants = std::variant<
 
 using index_variants = std::variant<py::array_t<int32_t>, py::array_t<int64_t>>;
 
-array_variants get_array(py::array &arr, const std::string &name,
-                         const int ndim = -1) {
-  if (ndim >= 0 && arr.ndim() != ndim) {
-    throw std::invalid_argument("Array " + name + " must be " +
-                                std::to_string(ndim) + "-dimensional");
-  }
+array_variants get_array(py::array &arr) {
   const int type_num = arr.dtype().num();
   switch (type_num) {
   case py::detail::npy_api::NPY_BOOL_:
@@ -118,70 +113,72 @@ ReturnState check_arrays(X x, Y y, const npy_intp n_row, const npy_intp n_col) {
 }
 
 template <typename ScalarType>
-ReturnState csr_matvec_runtime(const bool overwrite, const npy_intp n_col,
-                               const npy_intp n_row, const ScalarType alpha,
-                               py::array py_indptr, py::array py_indices,
-                               py::array py_data, py::array py_x,
-                               py::array py_y) {
+ReturnState csr_matvec(const bool overwrite, const npy_intp n_col,
+                       const npy_intp n_row, const ScalarType alpha,
+                       py::array py_indptr, py::array py_indices,
+                       py::array py_data, py::array py_x, py::array py_y) {
 
-  index_variants indptr = get_index(py_indptr, "indptr");
-  index_variants indices = get_index(py_indices, "indices");
+  index_variants indptr = get_array(py_indptr, "indptr");
+  index_variants indices = get_array(py_indices, "indices");
   array_variants data = get_array(py_data, "data", 1);
   array_variants x = get_array(py_x, "x");
   array_variants y = get_array(py_y, "y");
 
-  return std::visit([&overwrite, &n_row, &n_col,
-                     &alpha](const auto &indptr, const auto &indices,
-                             const auto &data, const auto &y, auto &x) {
-    using indptr_t = typename decltype(indptr)::value_type;
-    using indices_t = typename decltype(indices)::value_type;
-    using data_t = typename decltype(data)::value_type;
-    using y_t = typename decltype(y)::value_type;
-    using x_t = typename decltype(x)::value_type;
-    using result_t = result_type_t<data_t, std::decay_t<decltype(alpha)>, x_t>;
+  return std::visit(
+      [&overwrite, &n_row, &n_col,
+       &alpha](const auto &indptr, const auto &indices, const auto &data,
+               const auto &y, auto &x) {
+        using indptr_t = typename decltype(indptr)::value_type;
+        using indices_t = typename decltype(indices)::value_type;
+        using data_t = typename decltype(data)::value_type;
+        using y_t = typename decltype(y)::value_type;
+        using x_t = typename decltype(x)::value_type;
+        using alpha_t = std::decay_t<decltype(alpha)>;
+        using result_t = result_type_t<data_t, alpha_t, x_t>;
 
-    auto return_state = check_arrays(x, y, n_row, n_col);
+        auto return_state = check_arrays(n_col, n_row, indptr, indices, data, x, y);
 
-    if (return_state != ReturnState::SUCCESS) {
-      return return_state;
-    }
-
-    if constexpr (!std::is_same_v<x_t, y_t>) {
-      return ReturnState::MISMATCH_IN_OUT_TYPES;
-    } else if constexpr (!std::is_same_v<x_t, result_t>) {
-      return ReturnState::INVALID_OUT_TYPE;
-    } else if constexpr (!std::is_same_v<indptr_t, indices_t>) {
-      return ReturnState::MISMATCH_INDEX_TYPES;
-    } else {
-      ssize_t x_col = x.ndim() == 1 ? 1 : x.shape(1);
-
-      if (x_col == 1) {
-        if (indices.size() < 100) {
-          csr_matvec_noomp(overwrite, n_row, n_col, indptr.data(),
-                           indices.data(), data.data(), alpha, x.strides(0),
-                           x.data(), y.strides(0), y.mutable_data());
-        } else {
-          py::gil_scoped_release release;
-          csr_matvec_omp(overwrite, n_row, n_col, indptr.data(), indices.data(),
-                         data.data(), alpha, x.strides(0), x.data(),
-                         y.strides(0), y.mutable_data());
+        if (return_state != ReturnState::SUCCESS) {
+          return return_state;
         }
 
-      } else {
-        if (indices.size() < 100) {
-          csr_matvecs_noomp(overwrite, n_row, n_col, indptr.data(),
-                            indices.data(), data.data(), alpha, x.strides(0),
-                            x.strides(1), x.data(), y.strides(0), y.strides(1),
-                            y.mutable_data());
+        if constexpr (!std::is_same_v<x_t, y_t>) {
+          return ReturnState::MISMATCH_IN_OUT_TYPES;
+        } else if constexpr (!std::is_same_v<x_t, result_t>) {
+          return ReturnState::INVALID_OUT_TYPE;
+        } else if constexpr (!std::is_same_v<indptr_t, indices_t>) {
+          return ReturnState::MISMATCH_INDEX_TYPES;
         } else {
-          py::gil_scoped_release release;
-          csr_matvecs_noomp(overwrite, n_row, n_col, indptr.data(),
-                            indices.data(), data.data(), alpha, x.strides(0),
-                            x.strides(1), x.data(), y.strides(0), y.strides(1),
-                            y.mutable_data());
+          ssize_t x_col = x.ndim() == 1 ? 1 : x.shape(1);
+
+          if (x_col == 1) {
+            if (indices.size() < 100) {
+              csr_matvec_noomp(overwrite, n_row, n_col, indptr.data(),
+                               indices.data(), data.data(), alpha, x.strides(0),
+                               x.data(), y.strides(0), y.mutable_data());
+            } else {
+              py::gil_scoped_release release;
+              csr_matvec_omp(overwrite, n_row, n_col, indptr.data(),
+                             indices.data(), data.data(), alpha, x.strides(0),
+                             x.data(), y.strides(0), y.mutable_data());
+            }
+
+          } else {
+            if (indices.size() < 100) {
+              csr_matvecs_noomp(overwrite, n_row, n_col, indptr.data(),
+                                indices.data(), data.data(), alpha,
+                                x.strides(0), x.strides(1), x.data(),
+                                y.strides(0), y.strides(1), y.mutable_data());
+            } else {
+              py::gil_scoped_release release;
+              csr_matvecs_noomp(overwrite, n_row, n_col, indptr.data(),
+                                indices.data(), data.data(), alpha,
+                                x.strides(0), x.strides(1), x.data(),
+                                y.strides(0), y.strides(1), y.mutable_data());
+            }
+          }
+          return ReturnState::SUCCESS;
         }
-      }
-      return ReturnState::SUCCESS;
-    }
-  });
+      },
+      indptr, indices, data, y, x);
 }
