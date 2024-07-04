2c98685873cdb65c65c94deff34640e46b29e2d0
Phillip Weinberg <weinbe58@gmail.com>
2024-07-03 20:06:59 -0400

    updating script

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import tomlkit
 from packaging.version import Version
 import os
@@ -5,65 +6,155 @@ import argparse
 import git
 
 
-def write_version(version: Version):
-    with open(os.path.join("src", "parallel_sparse_tools", "_version.py"), "w") as f:
-        f.write(f'__version__ = "{version}"\n')
+class Executor:
+    def __init__(self, repo):
+        self.repo = repo
+        self._pyproject = None
 
-    with open("pyproject.toml", "r") as f:
-        pyproject = tomlkit.load(f)
+    @property
+    def pyproject(self):
+        if self._pyproject is None:
+            with open("pyproject.toml", "r") as f:
+                self._pyproject = tomlkit.load(f)
 
-    pyproject["project"]["version"] = str(version)
+        return self._pyproject
 
-    with open("pyproject.toml", "w") as f:
-        f.write(pyproject.as_string())
+    def get_version(self) -> Version:
+        self.og_version = self.pyproject["project"]["version"]
+
+        return self.og_version
+
+    def bump_patch(self) -> None:
+        assert not self._updated, "Version already updated"
+        self._updated = True
+
+        major, minor, patch = self.get_version().release
+        self.pyproject["project"]["version"] = f"{major}.{minor}.{patch + 1}"
+
+    def bump_minor(self) -> None:
+        assert not self._updated, "Version already updated"
+        self._updated = True
+
+        major, minor, patch = self.get_version().release
+        self.pyproject["project"]["version"] = f"{major}.{minor + 1}.{patch}"
+
+    def bump_major(self) -> None:
+        assert not self._updated, "Version already updated"
+        self._updated = True
+
+        major, minor, patch = self.get_version().release
+        self.pyproject["project"]["version"] = f"{major + 1}.{minor}.{patch}"
+
+    def write_version_info(self) -> None:
+        assert self._is_context, "Version not updated"
+
+        version = self.pyproject["project"]["version"]
+
+        with open(
+            os.path.join("src", "parallel_sparse_tools", "_version.py"), "w"
+        ) as f:
+            f.write(f'__version__ = "{version}"\n')
+
+        with open("pyproject.toml", "w") as f:
+            f.write(self.pyproject.as_string())
+
+        self._written = True
+        self.commit()
+
+    def commit(self) -> None:
+        assert self._written, "Version not updated"
+        self.repo.git.add("pyproject.toml")
+        self.repo.git.add("src/parallel_sparse_tools/_version.py")
+        self.repo.git.commit("-m", f"Release {self.pyproject['project']['version']}")
+
+        self._committed = True
+        self.tag()
+
+    def tag(self) -> None:
+        assert self._committed, "Version not committed"
+        self._tagged = True
+        version = self.pyproject["project"]["version"]
+        self.repo.create_tag(f"v{version}")
+
+        self.push()
+
+    def push(self) -> None:
+        assert self._tagged, "Tag not created"
+        self.repo.git.push()
+        self.repo.git.push("--tags")
+
+    def run_updates(self, number: Literal["patch", "minor", "major"]) -> None:
+        if number == "patch":
+            self.bump_patch()
+        elif number == "minor":
+            self.bump_minor()
+        elif number == "major":
+            self.bump_major()
+        else:
+            raise ValueError(f"Invalid option: {number}")
+
+        new_version = self.pyproject["project"]["version"]
+        response = input(
+            f"Ready to release {self.og_version} -> {new_version}. Press enter (y) to continue..."
+        )
+
+        if response.lower() != "y":
+            print("Aborting")
+            return
+
+        self.write_version_info()
+
+    def get_branch(self) -> str:
+        output = self.repo.git.branch()
+        for branch in output.split("\n"):
+            if branch.startswith("*"):
+                return branch[2:]
+
+        raise ValueError("No branch found")
+
+    def __enter__(self) -> "Executor":
+        self._is_context = True
+        self._written = False
+        self._committed = False
+        self._tagged = False
+        self._orginal_branch = self.get_branch()
+
+        try:
+            self.repo.git.stash()
+            if self._orginal_branch != "main":
+                self.repo.git.checkout("main")
+                self.repo.git.pull()
+
+            return self
+        finally:
+            if self._orginal_branch != "main":
+                self.repo.git.checkout(self._orginal_branch, force=True)
+
+            self.repo.git.stash("pop")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
 
 
 parser = argparse.ArgumentParser(
     prog="Publshing script", description="This script is used to publish the package"
 )
 
-parser.add_argument("version", type=str, help="The version to publish")
 parser.add_argument(
-    "--dry-run", action="store_true", help="Dry run the publishing process"
+    "number",
+    type=str,
+    choices=["patch", "minor", "major"],
+    help="The number to bump, either 'patch', 'minor' or 'major'",
 )
-
-
-def exec_udpate(repo, args):
-    version = Version(args.version)
-
-    write_version(version)
-
-    if not args.dry_run:
-        raise NotImplementedError("Publishing is not implemented yet")
-
-
-def get_branch(repo):
-    output = repo.git.branch()
-    for branch in output.split("\n"):
-        if branch.startswith("*"):
-            return branch[2:]
-
-    raise ValueError("No branch found")
 
 
 def main():
     repo = git.Repo()
-    branch = get_branch(repo)
 
-    try:
-        repo.git.stash()
-        if branch != "main":
-            repo.git.checkout("main")
-            repo.git.pull()
+    args = parser.parse_args()
 
-        args = parser.parse_args()
-
-        exec_udpate(repo, args)
-    finally:
-        if branch != "main":
-            repo.git.checkout(branch, force=True)
-
-        repo.git.stash("pop")
+    with Executor(repo) as executor:
+        executor.run_updates(args.number)
 
 
 if __name__ == "__main__":
